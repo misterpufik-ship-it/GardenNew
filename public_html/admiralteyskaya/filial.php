166ea51942eeac629e9fcbbfcd13c55ad0db92fa
misterpufik-ship-it <misterpufik-ship-it@users.noreply.github.com>
2026-06-02 12:49:43 +0300

    Refine branch gallery and footer layout

--- public_html/admiralteyskaya/filial.php
+++ public_html/admiralteyskaya/filial.php
@@ -1,11 +1,16 @@
 <?php require_once( 'couch/cms.php' ); ?>
 <cms:template title='Управление: Филиал' name='final_section' executable='0' order='70'>
-    
+
     <cms:editable name='final_group_info' label='Информация о филиале' type='group' />
     <cms:editable name='final_title' label='Название филиала' group='final_group_info' type='text'>Garden Lounge Udelnaya</cms:editable>
     <cms:editable name='final_subtitle' label='Подзаголовок' group='final_group_info' type='text'>Второй филиал тайного сада</cms:editable>
-    <cms:editable name='final_img' label='Изображение филиала' group='final_group_info' type='image'>https://garden-lounge.pro/img/akkuratova.webp</cms:editable>
-    
+    <cms:editable name='final_img' label='Обложка филиала' group='final_group_info' type='image'>https://garden-lounge.pro/img/akkuratova.webp</cms:editable>
+
+    <cms:repeatable name='final_gallery' label='Галерея филиала' group='final_group_info'>
+        <cms:editable name='final_gallery_img' label='Фото' type='image' />
+        <cms:editable name='final_gallery_alt' label='Подпись / alt' type='text' />
+    </cms:repeatable>
+
     <cms:editable name='final_address' label='Адрес' group='final_group_info' type='text'>СПб., ул. Аккуратова, д. 13</cms:editable>
     <cms:editable name='final_metro' label='Метро' group='final_group_info' type='text'>м. Удельная</cms:editable>
 
